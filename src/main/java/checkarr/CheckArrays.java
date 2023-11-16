9f8f285e509bfa2c531d19420c74e3001de6fcf0
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2023-11-16 21:54:08 +0300

    hm22 исправлено

--- src/main/java/checkarr/CheckArrays.java
+++ src/main/java/checkarr/CheckArrays.java
@@ -30,7 +30,6 @@ public class CheckArrays {
     public static boolean onlyOneAndTwo(int[] arr) {
         int one = 0;
         int two = 0;
-        boolean otherVal = false;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -38,20 +37,13 @@ public class CheckArrays {
             } else if (arr[i] == 2) {
                 two = 2;
             } else {
-                otherVal = true;
+                return false;
             }
         }
 
-        if (one == 1 && two == 2 && !otherVal) {
+        if (one == 1 && two == 2) {
             return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        int[] test = {1, 2, 1, 2, 2};
-        for (int i = 0; i < retArrWithValAfterOne(test).length; i++) {
-            System.out.println(retArrWithValAfterOne(test)[i]);
-        }
     }
 }
--- src/test/java/testarr/TestArrays.java
+++ src/test/java/testarr/TestArrays.java
@@ -3,55 +3,43 @@ package testarr;
 import checkarr.CheckArrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class TestArrays {
-    int[] int1 = {1, 2, 1, 2, 2};
-    int[] int2 = {2, 2};
-
-    int[] int3 = {1, 1, 1};
-    int[] int4 = new int[0];
-
-    int[] int5 = {3, 4, 5, 1};
-
-    @Test
-    public void testRetArrWithValAfterOneCaseOne() {
-        Assertions.assertArrayEquals(int2, CheckArrays.retArrWithValAfterOne(int1));
+    @MethodSource("addTestArrWithValAfterOne")
+    @ParameterizedTest
+    public void testRetArrWithValAfterOne(int[] arr1, int[] arr2) {
+       Assertions.assertArrayEquals(arr2, CheckArrays.retArrWithValAfterOne(arr1));
     }
 
-    @Test
-    public void testRetArrWithValAfterOneCaseTwo() {
-        Assertions.assertArrayEquals(int4, CheckArrays.retArrWithValAfterOne(int3));
+    public static Stream<Arguments> addTestArrWithValAfterOne() {
+        List<Arguments> out = new ArrayList<>();
+        out.add(Arguments.arguments(new int[]{1, 2, 1, 2, 2}, new int[]{2, 2}));
+        out.add(Arguments.arguments(new int[]{1, 1, 1}, new int[0]));
+        out.add(Arguments.arguments(new int[]{3, 4, 5, 1}, new int[0]));
+        return out.stream();
     }
-
     @Test
-    public void testRetArrWithValAfterOneCaseThree() {
-        Assertions.assertArrayEquals(int4, CheckArrays.retArrWithValAfterOne(int5));
+    public void testAllMethods() {
+        Assertions.assertAll(
+                () -> {
+                    Assertions.assertThrows(RuntimeException.class, () -> CheckArrays.retArrWithValAfterOne(new int[]{2, 2}));
+                },
+                () -> {
+                    Assertions.assertTrue(CheckArrays.onlyOneAndTwo(new int[]{1, 2, 1, 2, 2}));
+                },
+                () -> {
+                    Assertions.assertFalse(CheckArrays.onlyOneAndTwo(new int[]{1, 1, 1}));
+                },
+                () -> {
+                    Assertions.assertFalse(CheckArrays.onlyOneAndTwo(new int[0]));
+                }
+        );
     }
-
-    @Test
-    public void testRetArrWithValAfterOneCaseException() {
-        boolean ex = false;
-        try {
-            CheckArrays.retArrWithValAfterOne(int2);
-        } catch (RuntimeException e) {
-            ex = true;
-        }
-        Assertions.assertTrue(ex);
-    }
-
-    @Test
-    public void testOnlyOneAndTwoCaseOne() {
-        Assertions.assertTrue(CheckArrays.onlyOneAndTwo(int1));
-    }
-
-    @Test
-    public void testOnlyOneAndTwoCaseTwo() {
-        Assertions.assertFalse(CheckArrays.onlyOneAndTwo(int3));
-    }
-
-    @Test
-    public void testOnlyOneAndTwoCaseThree() {
-        Assertions.assertFalse(CheckArrays.onlyOneAndTwo(int4));
-    }
-
 }
